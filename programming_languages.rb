32612dee33935dbe8e8c7a6f9b4c7a0a8ade190c
John Shelby <jsnd11@gmail.com>
2017-01-03 14:02:08 -0500

    reformatted

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,47 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  multi_styles=[]
+  languages.each do |style,lang_hash|
+    lang_hash.each do |lang,info_hash|
+      multi_styles<<style
+      if new_hash.has_key?(lang)
+          multi_styles<<style
+        info_hash[:style]=multi_styles
+      else
+        info_hash[:style]=[style]
+    end
+      new_hash[lang]=info_hash
+  end
+end
+new_hash
 end
